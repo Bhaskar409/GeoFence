43561c3c4cae77c4485e06913e6609035a9d4d01
Bhaskar Kumar <bhaskar77277@gmail.com>
2026-02-19 19:06:58 +0530

    updated the js to dynamic for now its dynamic one not continuous

--- device-frontend/index.js
+++ device-frontend/index.js
@@ -1,15 +1,13 @@
 console.log("SCRIPT LOADED");
 
-let intervalId = null;
+
 function startTracking() {
-    const deviceId = document.getElementById("deviceId").value;
+    const deviceId = Number(document.getElementById("deviceId").value);
 
     if (!deviceId) {
-        alert("Enter Device ID");
+        alert("Enter valid Device ID");
         return;
     }
-
-    console.log("Button clicked");
 
     const data = {
         deviceId: deviceId,
@@ -17,7 +15,7 @@ function startTracking() {
         longitude: 77.2090
     };
 
-    console.log("Sending data:", data);
+    console.log("Sending:", JSON.stringify(data));
 
     fetch("http://localhost:8080/update", {
         method: "POST",
@@ -27,13 +25,12 @@ function startTracking() {
         body: JSON.stringify(data)
     })
     .then(res => {
-        console.log("Response status:", res.status);
+        console.log("Status:", res.status);
         return res.json();
     })
     .then(response => {
-        console.log("Server response:", response);
+        console.log("Response:", response.status);
+        alert("Device is " + response.status);
     })
-    .catch(err => {
-        console.error("Fetch error:", err);
-    });
+    .catch(err => console.error(err));
 }

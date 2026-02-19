92dba9235e815822888e3b32d6785b8f2cf0f3b2
Bhaskar Kumar <bhaskar77277@gmail.com>
2026-02-19 18:52:41 +0530

    The background clr to black

--- device-frontend/index.js
+++ device-frontend/index.js
@@ -9,55 +9,31 @@ function startTracking() {
         return;
     }
 
-    if (!navigator.geolocation) {
-        alert("Geolocation not supported by your browser");
-        return;
-    }
+    console.log("Button clicked");
 
-    console.log("Requesting location...");
+    const data = {
+        deviceId: deviceId,
+        latitude: 28.6139,
+        longitude: 77.2090
+    };
 
-    navigator.geolocation.getCurrentPosition(
-        function (position) {
-            console.log("Location received");
+    console.log("Sending data:", data);
 
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            const altitude = position.coords.altitude;
-
-            console.log("Lat:", latitude);
-            console.log("Lng:", longitude);
-            console.log("Alt:", altitude);
-
-            fetch("http://localhost:8080/update", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    deviceId: deviceId,
-                    latitude: latitude,
-                    longitude: longitude
-                })
-            })
-            .then(res => res.json())
-            .then(data => console.log("Server:", data))
-            .catch(err => console.error("Fetch error:", err));
+    fetch("http://localhost:8080/update", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
         },
-        function (error) {
-            console.error("Geolocation error:", error);
-
-            if (error.code === 1) {
-                alert("Permission denied. Allow location access.");
-            } else if (error.code === 2) {
-                alert("Position unavailable.");
-            } else if (error.code === 3) {
-                alert("Location request timed out.");
-            }
-        },
-        {
-            enableHighAccuracy: true,
-            timeout: 10000,
-            maximumAge: 0
-        }
-    );
+        body: JSON.stringify(data)
+    })
+    .then(res => {
+        console.log("Response status:", res.status);
+        return res.json();
+    })
+    .then(response => {
+        console.log("Server response:", response);
+    })
+    .catch(err => {
+        console.error("Fetch error:", err);
+    });
 }
